711fcf36aa0a40d0be1cbbd320de6c96dc6ae44e
ThunderGrove <rene699f@edu.easj.dk>
2020-02-18 11:37:05 +0100

    Datasplit

--- app/src/main/java/dk/rtlund/basiccalculator/MainActivity.java
+++ app/src/main/java/dk/rtlund/basiccalculator/MainActivity.java
@@ -3,12 +3,48 @@ package dk.rtlund.basiccalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    EditText editText;
+    TextView textView;
+    Button button;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.editText);
+        textView=findViewById(R.id.resultLabel);
+        button=findViewById(R.id.calc);
+    }
+
+    public void calc(View arg0){
+        String dataInput=editText.getText().toString();
+
+        ArrayList<String>data=new ArrayList<>();
+
+        int lastAt=0;
+
+        for(int i=0;i<dataInput.length();i++){
+            if(dataInput.charAt(i)=='+'){
+                data.add(dataInput.substring(lastAt,i-1));
+                lastAt=i+1;
+                data.add(dataInput.substring(i,i));
+            }
+        }
+
+        for (String string : data){
+            //System.out.println(string);
+            Log.d("dataTest",string);
+        }
     }
 }
